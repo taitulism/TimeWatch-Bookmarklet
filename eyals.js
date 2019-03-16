151c5399fe24a4b5905434b3297111f8ebfd70dd
Taitu Lizenbaum <email@gmail.com>
2019-03-16 15:22:59 +0200

    init

---
+++ eyals.js
@@ -0,0 +1,42 @@
+function go () {
+    const rows2 = document.querySelectorAll('table table:nth-child(1) tr:nth-child(n+4)')
+
+    const times = Array.from(rows2).map(function (row) {
+        const timeSpan = row.querySelector('td:nth-of-type(13)').textContent;
+        return timeSpan;
+    }).filter(function (timeSpan) {
+        return timeSpan && timeSpan.trim() && !timeSpan.startsWith("Missing");
+    })
+
+    const minutes = times.map(function (ts) {
+        return getMinutes(ts);
+    });
+
+    const expectedMinutes = times.length * 9 * 60;
+    const actualMinutes = minutes.reduce(function (sum, num) { return sum + num }, 0);
+    const asd = actualMinutes - expectedMinutes;
+    const hoursDifference = Math.abs(Math.floor(asd / 60));
+    const minutesDifference = Math.abs(asd % 60);
+    const hoursDifferenceString = hoursDifference < 10 ? ('0' + hoursDifference) : hoursDifference;
+    const minutesDifferenceString = minutesDifference < 10 ? ('0' + minutesDifference) : minutesDifference;
+    
+    if (asd > 0) {
+        console.log('%cYou are ahead by ' + hoursDifferenceString + ':' + minutesDifferenceString, 'background: green');
+    }
+    else {
+        console.log('%cYou are behind by ' + hoursDifferenceString + ':' + minutesDifferenceString, 'background: red');
+    }
+}
+
+function getMinutes(timeSpan) {
+    const parts = timeSpan.split(":");
+    return Number(parts[1]) + (Number(parts[0]) * 60);
+}
+
+function getDate(time) {
+    const hour = time.substr(0, time.indexOf(':'));
+    const minutes = time.substr(time.indexOf(':') + 1);
+    return new Date(2019, 0, 1, hour, minutes);
+}
+
+go();
---
+++ index.js
@@ -0,0 +1,53 @@
+if (window.location.hostname !== 'checkin.timewatch.co.il') window.location.href = 'https://checkin.timewatch.co.il';
+
+const trs = jQuery('.tr td:nth-child(13)');
+
+const mapped = trs.map(function (i, td) {
+    const txt = td.textContent.substr(1);
+
+    if (!txt || txt.substr(0, 4) === 'issi') return null;
+
+    const time = txt.split(':').map(function (x){
+        return parseInt(x, 10)
+    });
+    const hours = time[0];
+    const minutes = time[1];
+
+    var minLeft;
+    var minExtra;
+
+    if (hours < 9) { // missing
+        const hoursLeft = 9 - hours - 1;
+
+        minLeft = (hoursLeft * 60) + (60 - minutes);
+    }
+    else if (hours >= 9) { // extra
+        const hoursExtra = hours - 9;
+
+        minExtra = (hoursExtra * 60) + minutes;
+    }
+
+    const totalMinutes = minLeft ? (minLeft * -1) : minExtra;
+
+    return totalMinutes;
+}).get();
+
+const reduced = mapped.reduce(function (x, v) {
+    return x+v;
+}, 0)
+
+const sign = reduced < 0 ? '-' : '+';
+
+const diffTime = reduced < 0 ? (reduced * -1) : reduced;
+
+const remain = diffTime % 60;
+
+if (!remain) {
+    console.log(diffTime / 60 + ':00')
+}
+else {
+    const hoursLeft = Math.floor(diffTime/60);
+    const minsLeft = diffTime%60;
+
+    alert(sign + hoursLeft + ':' + minsLeft);
+}
