f4f5fb36c3667153b81f31d32cb9fafb7286215e
Itai Tenenbaum <taitulism@gmail.com>
2019-03-18 00:50:52 +0200

    script loaded bookmarklet (no size limit)

---
+++ script-loader.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.src='https://cdn.jsdelivr.net/gh/taitulism/TimeWatch-Bookmarklet/index.js';
+script.type='text/javascript';
+document.body.appendChild(script);
