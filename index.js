60e33a6661d2b2c427be5ccbfeae7c28fd7e5cd2
Itai Tenenbaum <taitulism@gmail.com>
2019-03-17 02:58:41 +0200

    WIP

--- index.js
+++ index.js
@@ -5,55 +5,76 @@
         return;
     }
     
+    var COLUMN_TITLE = 'Total Hours';
+    var EXPECTED_HOURS_PER_DAY = 9;
+
+    var dailyTotalExpectedMinutes = EXPECTED_HOURS_PER_DAY * 60;
     var table = document.querySelectorAll('table table')[4];
-    var rows = table.querySelectorAll('tr:nth-child(n+4)');
+    var allRows = table.querySelectorAll('tr');
+    // var rows = table.querySelectorAll('tr:nth-child(n+4)');
+    // var firstRow = table.querySelector('tr');
+    var firstTitleRow = allRows[0];
+    var SecondTitleRow = allRows[1];
+    var dataRows = allRows.slice(3);
     
-    var mapped = Array.from(rows).map(function (i, td) {
-        const txt = td.textContent.substr(1);
 
-        if (!txt || txt.substr(0, 4) === 'issi') return null;
+    // Automatically find the right column
+    var totalHoursColumnIndex = Array.from(firstTitleRow.children).findIndex(function (title) {
+        return title.textContent === COLUMN_TITLE;
+    });
 
-        const time = txt.split(':').map(function (x){
+    // Additional column offset
+    // -1-     -2-     -3-
+    // in|out  in|out  in|out
+    totalHoursColumnIndex + SecondTitleRow.children.length * 2;
+    
+    var totalActualMinutes = Array.from(dataRows).map(function (day, i) {
+        var dailyTotal = day.querySelector('td:nth-child('+ totalHoursColumnIndex +')').textContent.trim();
+
+        if (!dailyTotal || dailyTotal.startsWith("Missing")) return null;
+  
+        var actualTime = dailyTotal.split(':').map(function (x) {
             return parseInt(x, 10)
         });
-        const hours = time[0];
-        const minutes = time[1];
+        var actualHours = actualTime[0];
+        var actualMinutes = actualTime[1];
+        var actualTotalMinutes = (actualHours * 60) + actualMinutes;
 
         var minLeft;
         var minExtra;
 
-        if (hours < 9) { // missing
-            const hoursLeft = 9 - hours - 1;
+        if (actualHours < EXPECTED_HOURS_PER_DAY) { // missing
+            var hoursLeft = EXPECTED_HOURS_PER_DAY - actualHours - 1;
 
-            minLeft = (hoursLeft * 60) + (60 - minutes);
+            minLeft = (hoursLeft * 60) + (60 - actualMinutes);
         }
-        else if (hours >= 9) { // extra
-            const hoursExtra = hours - 9;
+        else if (actualHours >= EXPECTED_HOURS_PER_DAY) { // extra
+            var hoursExtra = actualHours - EXPECTED_HOURS_PER_DAY;
 
-            minExtra = (hoursExtra * 60) + minutes;
+            minExtra = (hoursExtra * 60) + actualMinutes;
         }
 
-        const totalMinutes = minLeft ? (minLeft * -1) : minExtra;
+        var totalMinutes = minLeft ? (minLeft * -1) : minExtra;
 
         return totalMinutes;
-    }).get();
+    })
+    .filter(function (minutes) {
+        return typeof minutes === 'number';       
+    })
+    .reduce(function (minutesTotal, minutesToday) {
+        return minutesTotal + minutesToday;
+    }, 0);
 
-    const reduced = mapped.reduce(function (x, v) {
-        return x+v;
-    }, 0)
-
-    const sign = reduced < 0 ? '-' : '+';
-
-    const diffTime = reduced < 0 ? (reduced * -1) : reduced;
-
-    const remain = diffTime % 60;
+    var sign = totalActualMinutes < 0 ? '-' : '+';
+    var diffTime = totalActualMinutes < 0 ? (totalActualMinutes * -1) : totalActualMinutes;
+    var remain = diffTime % 60;
 
     if (!remain) {
         console.log(diffTime / 60 + ':00')
     }
     else {
-        const hoursLeft = Math.floor(diffTime/60);
-        const minsLeft = diffTime%60;
+        var hoursLeft = Math.floor(diffTime/60);
+        var minsLeft = diffTime%60;
 
         alert(sign + hoursLeft + ':' + minsLeft);
     }
